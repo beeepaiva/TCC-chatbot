34e6f671be1ed9361bff381a8fc9829b889a286e
Beatriz Paiva <beeepaiva@gmail.com>
2022-03-10 17:25:28 -0300

    Creating inteface propmt

--- app.py
+++ app.py
@@ -26,13 +26,7 @@ import tflearn
 import tensorflow as tf
 import random
 
-#carregando o modelo 
-net = tflearn.input_data(shape=[None, len(train_x[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-net = tflearn.regression(net)
-model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
 
 
 def clean_up_sentence(sentence):
@@ -57,4 +51,49 @@ def bow(sentence, words, show_details=False):
 
     return(np.array(bag))
 
+ERROR_THRESHOLD = 0.25
+def classify(sentence):
+    # generate probabilities from the model
+    results = model.predict([bow(sentence, words)])[0]
+    # filter out predictions below a threshold
+    results = [[i, r] for i, r in enumerate(results) if r > ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    # return tuple of intent and probability
+    return return_list
+
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    # if we have a classification then find the matching intent tag
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # find a tag matching the first results
+                if i['tag'] == results[0][0]:
+                    # a random response from the intent
+                    return print(f"reposta: {random.choice(i['responses'])}, tag: {results[0][0]}, prob: {results[0][1]}")
+
+            results.pop(0)    
+
+#carregando o modelo 
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 model.load('./model.tflearn')
+
+print("Em que posso ajudar?")
+print("Para encerrar digite 'sair'")
+
+
+while True:
+    entrada = input("Você: ")
+    if entrada == "sair":
+        break
+    response(entrada)
