5abd928f888f2448cd5033191b52044e3e47529a
Beatriz Paiva <beeepaiva@gmail.com>
2022-04-12 20:44:06 -0300

    Repostas dinamicas

--- chat_interface.py
+++ chat_interface.py
@@ -15,8 +15,14 @@ from nlp_inicial import bagOfWords, tokenizacao, spacyEntities
 from nltk.stem.rslp import RSLPStemmer
 import pandas as pd
 import openpyxl
-from word2number import w2n
-from datetime import date
+from word2numberi18n import w2n
+from datetime import date, timedelta
+from text_to_num import text2num
+from text_to_num import alpha2digit
+from text_to_num.lang.portuguese import OrdinalsMerger
+
+omg = OrdinalsMerger()
+USE_PT_ORDINALS_MERGER = True
 
 stPortugues = RSLPStemmer()
 
@@ -25,6 +31,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 with open('./database/intents.json', encoding='utf-8') as f:
     intents = json.load(f)
     
+instance = w2n.W2N(lang_param="pt")  
 FILE = "./database/data.pth"
 data = torch.load(FILE)
 
@@ -42,6 +49,7 @@ model.eval()
 bot_name = "Bea"
 context = ""
 conversation = {}
+entitiesStorage = {}
 
 def get_response(msg):
     sentence = tokenizacao(msg)
@@ -63,45 +71,63 @@ def get_response(msg):
     
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
+    
     conversation.update({tag: msg})
     
+    
+
+    ###### SE FOR DESCOBRIR SOBRE SALA, HORARIO OU AULAS
     if prob.item() > 0.75:
         intencao = tag
         ##Deve estar presente 
         ## - Semestre e dia
+        #if "dia" not in entitiesStorage.keys():
         if "dia" in entitiesQuestion.keys():
-            dia = entitiesQuestion["dia"]
+            ### Tratativa de datas (Tem outra forma????)
+            if entitiesQuestion["dia"] == "amanhã":
+                dia = date.today()+ timedelta(days=1)
+            if entitiesQuestion["dia"] == "hoje":
+                dia = date.today()
+                #dia = entitiesQuestion["dia"]
         else:    
             dia = date.today()
 
+    #    entitiesStorage.update({'dia': dia})
+
+        #if "semestre" not in entitiesStorage.keys():
         if "semestre" in entitiesQuestion.keys():
             turma = entitiesQuestion["semestre"]
         else:
-            return f"Qual seu semestre?"
-    
-        ## PROCURA NO EXCEL
-        wb = openpyxl.load_workbook('./database/database_responses.xlsx')
-        ws = wb["Planilha1"]
+            return f"Digite o número do seu semestre?"
 
-        list_with_values=[]
-        for cell in ws[1]:
-            list_with_values.append((cell.value).lower())
+        #entitiesStorage.update({'semestre': turma})
+        ## PROCURA NO EXCEL
 
         df = pd.read_excel('./database/database_responses.xlsx')
         values = df[(df['Semestre'] == int(turma)) & (df['Dia'] == dia.strftime("%Y-%m-%d"))]
         print(values)
 
-        if not values.empty():
-            array_response = []
-            for val in values:
-                array_response.append(val)
-
         ## DESCOBRIR A INTENÇÃO PRINCIPAL E TRAZER SOMENTE A COLUNA DESEJADA 
+        if not values.empty:
+            i = 0
+            arrayResponse = {}
+            array_responseAula = {}
+            array_responseSala = {}
+            array_responseHorario = {}
+            while i < len(values.index):
+                arrayResponse.update({
+                    "Aula": values['Aula'].values[i],
+                    "Sala": values['Sala'].values[i],
+                    "Horario":""
+                })
+                i += 1
+        
 
         if prob.item() > 0.75:
             for intent in intents["intents"]:
-                if tag == intent["tag"]:
+                if next(iter(conversation)) == intent["tag"]:
                         #Retornando a mensagem, a tag(intencao) e probabilidade da resposta
-                    return f"Mensagem: {random.choice(intent['responses'])}, tag: {intent[tag]}, prob: {prob.item()}"
+                    conversation.clear()
+                    return f"Mensagem: {intent['responses']} {arrayResponse['Aula'].lower()}, na sala {arrayResponse['Sala']}, tag: {intent['tag']}, prob: {prob.item()}"
         else:
             return f"Não entendi"
