2e191c9315e8dfa3ea496734683f9c9699573ecf
Beatriz Paiva <beeepaiva@gmail.com>
2022-04-22 23:06:45 -0300

    Refatorando codigo e resposta

--- chat_interface.py
+++ chat_interface.py
@@ -16,6 +16,8 @@ import pandas as pd
 from datetime import date
 from actions import convertNum, convertDate
 import numpy
+import string
+import os
 
 
 stPortugues = RSLPStemmer()
@@ -39,8 +41,6 @@ model = Neural(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Bea"
-context = ""
 conversation = {}
 entitiesStorage = {}
 
@@ -50,14 +50,12 @@ def get_response(msg):
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
     
+    # dict para identificar entidade e seu valor na mensagem
     entitiesQuestion = {}
     doc = spacyEntities(msg)
     for ent in doc.ents:
         entitiesQuestion.update({ent.label_: ent.text})
 
-    # o que o usuario quer
-    # entitiesQuestion[]
-    
     output = model(X)
     _, predicted = torch.max(output, dim=1)
     tag = tags[predicted.item()]
@@ -66,115 +64,100 @@ def get_response(msg):
     prob = probs[0][predicted.item()]
     
     conversation.update({tag: msg})
-
    
+   ## Se tiver mais de 65% de chance de ser a intenção, ele responde
     if prob.item() > 0.65:
-        intencao = tag
         
         ###### SE FOR DESCOBRIR SOBRE SALA, HORARIO OU AULAS
         if {key:val for key, val in conversation.items() 
                    if key.startswith("descobrir_")}:
             ##Deve estar presente 
             ## - Semestre e dia
-            if "dia" not in entitiesStorage.keys():
-                if "dia" in entitiesQuestion.keys():
-                    dia = convertDate(entitiesQuestion["dia"])
-                    entitiesStorage['dia'] = dia
-                else:    
-                    dia = date.today()
-                    entitiesStorage['dia'] = dia
+            # Caso o dia não esteja presente, automaticamente pega o HOJE 
+            # Se o semestre não estiver presente, ele pergunta 
+            if "dia" not in entitiesStorage.keys() and "dia" in entitiesQuestion.keys():
+                dia = convertDate(entitiesQuestion["dia"])
+                entitiesStorage['dia'] = dia
+            else:    
+                dia = date.today()
+                entitiesStorage['dia'] = dia
 
-            if "semestre" not in entitiesStorage.keys():
-                if "semestre" in entitiesQuestion.keys():
-                    turma = convertNum(entitiesQuestion["semestre"])
-                    entitiesStorage['semestre'] = turma
-                else:
-                    return f"Qual seu semestre?"
+            if "semestre" not in entitiesStorage.keys() and "semestre" in entitiesQuestion.keys():
+                turma = convertNum(entitiesQuestion["semestre"])
+                entitiesStorage['semestre'] = turma
+            else:
+                return f"Qual seu semestre?"
 
             ## PROCURA NO EXCEL
             df = pd.read_excel('./database/database_responses.xlsx')
             values = df[(df['Semestre'] == int(entitiesStorage['semestre'])) & (df['Dia'] == entitiesStorage['dia'].strftime("%Y-%m-%d"))]
-
-            ## DESCOBRIR A INTENÇÃO PRINCIPAL E TRAZER SOMENTE A COLUNA DESEJADA 
             
             if not values.empty:
-                i = 0
-                j = 0
-                Aulas = []
-                Hor = []
-                Sala = []
-                Turmas = []
-                TurmaName = []
+                i, j = 0,0
+                # Arrays para salvar as aulas, salas, turmas e horarios que retornaram da pesquisa
+                Aulas, Horarios, Salas, Turmas = ([] for i in range(4))
+                # Para mais de uma turma 
+                TurmaID = []
                 while i < len(values.index):
                     while j < len(numpy.unique(values['Turma'])):
-                        name = numpy.unique(values['Turma'])[j]
-                        TurmaName.append(name[-1:])
+                        idTurma = numpy.unique(values['Turma'])[j]
+                        TurmaID.append(idTurma[-1:])
                         j+=1
                     Turmas.append(values['Turma'].values[i][-1:])
                     Aulas.append(values['Aula'].values[i])
-                    Sala.append(values['Sala'].values[i])
-                    Hor.append(values['Horário'].values[i])
+                    Salas.append(values['Sala'].values[i])
+                    Horarios.append(values['Horário'].values[i])
                     i += 1
+            else:
+                return f"Nada foi encontrado :("
 
+            #Limpa o contexto que salva as entidades da conversa e a conversa
             conversation.clear()
             entitiesStorage.clear()
-            lista = []
-            ## Se tiver mais de uma turma
-            if len(TurmaName) > 1:
-                message = ""
-                msgTurma = {}
-                index = 0 
-                while index < len(Aulas):
-                    teste = Turmas[index] + " |-| " + Aulas[index] + " |-| " + Sala[index] 
-                    if teste not in msgTurma.keys():
-                        msgTurma.update({teste:[]})
-                        msgTurma[teste].append(Hor[index])
-                    else:
-                        msgTurma[teste].append(Hor[index])
-                    index +=1
-                for key in msgTurma:                    
-                    HoraInicial = msgTurma[key][0].split("-")[0]
-                    HoraFinal = msgTurma[key][len(msgTurma[key])-1].split("-")[1]
-                    msgTurma[key] = {"HoraInicio": HoraInicial, "HoraFim": HoraFinal}
 
-                for key in msgTurma:
-                    turmam,aula,sala = key.split('|-|')
-                    message += f"Turma: {turmam} na sala {sala} tendo a aula {aula.capitalize()} das {msgTurma[key]['HoraInicio']} até {msgTurma[key]['HoraFim']}"        
-                    message += '\n'
-
+            ## Gerando resposta conforme quantidade de turmas
+            # id enviado corresponde ao tipo de mensagem que será criada 
+            # 1 - mais de uma turma e 0 - uma turma
+            if len(TurmaID) > 1:
+                message = createResponse(Aulas, Salas, Turmas, Horarios, 1)
                 return message
             else:
-                message = ""
-                msgTurma = {}
-                index = 0 
-                while index < len(Aulas):
-                    teste = Turmas[index] + " |-| " + Aulas[index] + " |-| " + Sala[index] 
-                    if teste not in msgTurma.keys():
-                        msgTurma.update({teste:[]})
-                        msgTurma[teste].append(Hor[index])
-                    else:
-                        msgTurma[teste].append(Hor[index])
-                    index +=1
-                for key in msgTurma:                    
-                    HoraInicial = msgTurma[key][0].split("-")[0]
-                    HoraFinal = msgTurma[key][len(msgTurma[key])-1].split("-")[1]
-                    msgTurma[key] = {"HoraInicio": HoraInicial, "HoraFim": HoraFinal}
-
-                for key in msgTurma:
-                    turmam,aula,sala = key.split('|-|')
-                    message += f"\nAula {aula.capitalize()}, na sala {sala} das {msgTurma[key]['HoraInicio']} até {msgTurma[key]['HoraFim']} \n"        
-                
+                message = createResponse(Aulas, Salas, Turmas, Horarios, 0)
                 return message
-            ## SE NAO FOR BUSCAR CONTEUDO DINAMICO
+        ## SE NAO FOR BUSCAR CONTEUDO DINAMICO
         else:
             for intent in intents["intents"]:
                 if tag == intent["tag"]:
-                    #return f"Mensagem: {random.choice(intent['responses'])}, tag: {intent['tag']}, prob: {prob.item()}"
                     return f"{random.choice(intent['responses'])}"
     else:
         return f"Não entendi, mas você provavelmente pode encontrar essa informação acessando o site! https://www.sp.senac.br/ :)"
 
 
-def createResponse(aula, sala, turma):
-    response = []
-    return response
+#Função que cria a mensagem de resposta. 
+# Recebe os Arrays, e o id para devolver com ou sem a turma
+def createResponse(aulasResponse, salasResponse, turmasResponse, horariosResponse, id):
+    message = ""
+    msgTurma = {}
+    z = 0 
+    while z < len(aulasResponse):
+        teste = turmasResponse[z] + " |-| " + aulasResponse[z] + " |-| " + salasResponse[z] 
+        if teste not in msgTurma.keys():
+            msgTurma.update({teste:[]})
+            msgTurma[teste].append(horariosResponse[z])
+        else:
+            msgTurma[teste].append(horariosResponse[z])
+        z +=1
+        
+    for key in msgTurma:                    
+        HoraInicial = msgTurma[key][0].split("-")[0]
+        HoraFinal = msgTurma[key][len(msgTurma[key])-1].split("-")[1]
+        msgTurma[key] = {"HoraInicio": HoraInicial, "HoraFim": HoraFinal}
+
+    for key in msgTurma:
+        turmaN,aula,sala = key.split('|-|')
+        if id == 1:
+            message += f' - Turma {turmaN} na sala {sala} tendo a aula {string.capwords(aula)} das {msgTurma[key]["HoraInicio"]} até {msgTurma[key]["HoraFim"]}<br>'
+        else:
+            message += f' - Aula {string.capwords(aula)}, na sala {sala} das {msgTurma[key]["HoraInicio"]} até {msgTurma[key]["HoraFim"]}{os.linesep}<br>'
+    
+    return message
